46fedd3d5b8d876143006870e54e5f911fc6961d
ljk8654 <132908803+ljk8654@users.noreply.github.com>
2026-04-29 12:42:55 +0900

    main에서 RUDAcitivity로 실행되게 함

--- app/src/main/java/kr/ac/tukorea/ljk/randomunitdefence/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/ljk/randomunitdefence/MainActivity.kt
@@ -1,20 +1,37 @@
 package kr.ac.tukorea.ljk.randomunitdefence
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kr.ac.tukorea.ljk.randomunitdefence.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+//        // 디버그 빌드일 때에는 1초 후 게임 화면으로 바로 넘어가게 한다
+//        if (BuildConfig.DEBUG) {
+//            Handler(Looper.getMainLooper()).postDelayed({
+//                startGameActivity()
+//            }, 1000)
+//        }
+        startGameActivity()
+    }
+
+    fun onBtnStartGame(view: View) {
+        startGameActivity()
+    }
+
+    private fun startGameActivity() {
+        val intent = Intent(this, RUDActivity::class.java)
+        startActivity(intent)
     }
 }
