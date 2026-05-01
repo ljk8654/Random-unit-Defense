408bf0fcd2ee6a5957b157a36011cd255a57e7ed
ljk8654 <132908803+ljk8654@users.noreply.github.com>
2026-05-01 17:36:19 +0900

    궁수 타워 마우스 클릭 후 이동하여 놓으면 배치

---
+++ app/src/main/java/kr/ac/tukorea/ljk/randomunitdefence/Archer.kt
@@ -0,0 +1,30 @@
+package kr.ac.tukorea.ljk.randomunitdefence
+
+import kr.ac.tukorea.ge.spgp2026.a2dg.objects.Sprite
+import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+import kr.ac.tukorea.ge.spgp2026.a2dg.R
+import kr.ac.tukorea.ge.spgp2026.a2dg.objects.AnimSprite
+import android.view.MotionEvent
+
+class Archer (gctx: GameContext): AnimSprite(gctx, kr.ac.tukorea.ljk.randomunitdefence.R.mipmap.archer, 10f, 6){
+
+    init {
+        width = Archer.WIDTH
+        height = Archer.HEIGHT
+        setCenter(move_x, move_y)
+    }
+
+    override fun update(gctx: GameContext) {
+        x = move_x
+        y = move_y
+        syncDstRect()
+    }
+
+    companion object{
+        const val WIDTH = 90f
+        const val HEIGHT = 200f
+        var move = false
+        var move_x = 600f
+        var move_y = 400f
+    }
+}
--- app/src/main/java/kr/ac/tukorea/ljk/randomunitdefence/MainScene.kt
+++ app/src/main/java/kr/ac/tukorea/ljk/randomunitdefence/MainScene.kt
@@ -5,10 +5,38 @@ import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
 import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
 import kr.ac.tukorea.ge.spgp2026.a2dg.scene.World
 import kr.ac.tukorea.ljk.randomunitdefence.R
+import kr.ac.tukorea.ge.spgp2026.a2dg.objects.Sprite
+import android.view.MotionEvent
+import kr.ac.tukorea.ljk.randomunitdefence.Archer.Companion.move
+import kr.ac.tukorea.ljk.randomunitdefence.Archer.Companion.move_x
+import kr.ac.tukorea.ljk.randomunitdefence.Archer.Companion.move_y
 
+enum class Layer{
+    BG, TOWER
+}
 class MainScene(gctx: GameContext) : Scene(gctx){
     override val clipsRect = true
-    override val world = World(arrayOf(0)).apply{
-        add(HorzScrollBackground(gctx, R.mipmap.tower_bg, 0f),0)
+    override val world = World(Layer.entries.toTypedArray()).apply{
+        add(HorzScrollBackground(gctx, R.mipmap.tower_bg, 0f), Layer.BG)
+        add(Archer(gctx), Layer.TOWER)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val pt = gctx.metrics.fromScreen(event.x,event.y)
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            if (pt.x in Archer.move_x - Archer.WIDTH/2 .. Archer.move_x + Archer.WIDTH/2 && pt.y in Archer.move_y - Archer.HEIGHT/2 .. move_y + Archer.HEIGHT/2){
+
+                Archer.move = true
+
+            }
+        }
+        if (event.action == MotionEvent.ACTION_MOVE && Archer.move){
+            move_x = pt.x
+            move_y = pt.y
+        }
+        if (event.action == MotionEvent.ACTION_UP){
+            Archer.move = false
+        }
+        return true
     }
 }
