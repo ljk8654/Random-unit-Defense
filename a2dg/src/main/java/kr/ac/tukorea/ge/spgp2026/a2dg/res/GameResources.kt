89ca388ea23d415030cc3715ebaf0536d16cd3d5
ljk8654 <132908803+ljk8654@users.noreply.github.com>
2026-05-06 20:55:32 +0900

    a2dg에 추가된 것들 추가함

--- a2dg/src/main/java/kr/ac/tukorea/ge/spgp2026/a2dg/res/GameResources.kt
+++ a2dg/src/main/java/kr/ac/tukorea/ge/spgp2026/a2dg/res/GameResources.kt
@@ -1,5 +1,6 @@
 package kr.ac.tukorea.ge.spgp2026.a2dg.res
 
+import android.content.Context
 import android.content.res.Resources
 import android.graphics.Bitmap
 
@@ -7,8 +8,10 @@ import android.graphics.Bitmap
 // 지금은 Bitmap 만 다루지만, 나중에는 소리나 폰트 같은 다른 리소스도 이쪽으로 모을 수 있다.
 class GameResources(
     resources: Resources,
+    context: Context
 ) {
     private val bitmapPool = BitmapPool(resources)
+    val sound = Sound(context)
 
     fun getBitmap(resId: Int): Bitmap {
         return bitmapPool.get(resId)
